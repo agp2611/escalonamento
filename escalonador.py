4431128076c7f5ca646d7f05222f4f43db01f631
ronaldo-urquiza <r.uhf@hotmail.com>
2026-06-23 03:36:49 -0300

    FCFS pronto

--- escalonador.py
+++ escalonador.py
@@ -1,24 +1,47 @@
 #Importando bibliotecas necessárias
-import math
 import string 
+from fcfs import rodar_fcfs
+from typing import List
+from typing import Dict, List
 
 #Inciailizando variáveis de forma limpa
 tipo_fila: str = ""
-retorno_medio: float = 0.0; resposta_medio: float = 0.0; espera_medio: float = 0.0
+retorno_FCFS: float = 0.0; resposta_FCFS: float = 0.0; espera_FCFS: float = 0.0
+processos: List[str] = []
+dicionario_de_processos: dict[str, any] = {}
 
-#Contém o tempo de chegada e a duração de cada processo
-# -=- Ref: https://www.w3schools.com/python/python_file_open.asp
-entrada = open("entrada.txt", "r") #Apenas leitura
+#Lendo os processos do arquivo de entrada - Contém o tempo de chegada e a duração de cada processo
+# -=- Ref: https://www.geeksforgeeks.org/python/python-read-text-file-into-list-or-array/
+with open("entrada.txt") as file:
+    processos = [line.strip() for line in file] #stip removes spaces at the start and end of the string.
+
+#Convertendo em dicionário python
+# -=- Ref: https://medium.com/@atatus/https-www-atatus-com-blog-python-converting-lsts-to-dictionaries-c3f038a8ce30
+for indice, elemento in enumerate(processos):
+    # elemento.split() transforma '0 20' em ['0', '20']
+    chegada, duracao = elemento.split() 
+    
+    # Guarda como tupla de inteiros: (0, 20)
+    dicionario_de_processos[indice] = (int(chegada), int(duracao))
+
+#Testando se o dicionário foi criado corretamente
+#print(dicionario_de_processos)
 
 """
 Estruturando saída: 
 
 [Tipo de Fila] [Tempo de Retorno médio] [Tempo de Resposta médio] [Tempo de Espera médio]
-
+                retorno = tempo_fim - tempo_chegada
+                                         resposta = tempo_primeira_execucao - tempo_chegada
+                                                                   espera = retorno - duracao
+                                        
 exemplo:
 FCFS 30,5 19,5 19,5 
 SJF 21,5 10,5 10,5
 RR 31,5 2,0 20,5
 """
 
-print("%s %.1f %.1f %.1f" % (tipo_fila, retorno_medio, resposta_medio, espera_medio))
+#Rodar FCFS
+retorno_FCFS, resposta_FCFS, espera_FCFS = rodar_fcfs(dicionario_de_processos)
+
+print("FCFS: %.1f %.1f %.1f" % (retorno_FCFS, resposta_FCFS, espera_FCFS))
---
+++ fcfs.py
@@ -0,0 +1,64 @@
+"""
+Exemplo para entendimento:
+
+Entrada:
+0 20
+0 10
+4 6
+4 8
+
+Total = 4 processos e 44 segundos
+tempo: 0----5----10----15----20----25----30----35----44
+P1:    [======================]
+P2:                           [===========]
+P3:                                       [======]
+P4:                                              [====]
+"""
+
+def rodar_fcfs(dicionario_de_processos):
+
+    #Inciailizando variáveis de forma limpa
+    retorno: float = 0.0; resposta: float = 0.0; espera: float = 0.0
+    chegada: float = 0.0; inicio: float = 0.0; fim: float = 0.0; duracao: float = 0.0
+    retorno_medio: float = 0.0; resposta_media: float = 0.0; espera_media: float = 0.0
+    dicionario_guia: dict[int, dict] = {}
+
+    #Loop percorrendo todos os processos do dicionário de entrada
+    for indice, (chegada, duracao) in dicionario_de_processos.items():
+
+        #Se for o primeiro processo então ele executa primeiro
+        if indice == 0: 
+            inicio = chegada
+        #Se não é o primeiro, veio depois, ele só começou quando o anterior terminou
+        else:
+            inicio = max(fim_anterior, chegada) #Chega antes do CPU ficar livre: então início só vai ser no momento que o anterior terminar
+                                                #Chega depois do CPU ficar livre:  então início só vai ser no momento que o processo atual chegar, independente do anterior ter terminado antes
+
+        #O fim de um processo é o momento que ele começou a executar + o tempo que ele demora para executar
+        fim = inicio + duracao
+
+        #Calculando variáveis de cada processo
+        dicionario_guia[indice] = {
+            "chegada": chegada,
+            "duracao": duracao,
+            "inicio": inicio,      # quando começou a executar pela 1ª vez
+            "fim": fim,            # quando terminou
+            "retorno": fim-chegada,   
+            "resposta": inicio-chegada,  
+            "espera": (fim - chegada) - duracao
+        }
+
+        #Adiciona valores de cada processo em variáveis externas para calcular a média no final
+        retorno_medio += dicionario_guia[indice]["retorno"]
+        resposta_media += dicionario_guia[indice]["resposta"]
+        espera_media += dicionario_guia[indice]["espera"]
+
+        #Onde o processo atual terminou pra o próximo saber onde começar
+        fim_anterior = fim  
+    
+    #Calculando médias com a ajuda do do dicionario de entrada, que contém a quantidade de processos, o len retorna quantos indíces (processos) existem no dicionário
+    retorno_medio /= len(dicionario_de_processos)
+    resposta_media /= len(dicionario_de_processos)
+    espera_media /= len(dicionario_de_processos)
+
+    return retorno_medio, resposta_media, espera_media
